8adc461e2873ea47b72a26215bd02fafc3542209
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2022-03-29 17:20:23 +0530

    Time: 39 ms (70.26%), Space: 27.6 MB (5.40%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -2,58 +2,44 @@ class Solution {
 public:
     
     
-    void merge(vector<int>& arr, int low, int mid, int high)
-    {
-        int n1 = (mid-low+1);
-        int n2 = (high-(mid+1)+1);
-        
-        vector<int> leftArr(n1);
-        vector<int> rightArr(n2);
-        
-        for(int i=0;i<n1;i++)
-            leftArr[i] = arr[i+low];
-        for(int i=0;i<n2;i++)
-            rightArr[i] = arr[mid+1+i];
-        
-        int i=0;
-        int j=0;
-        int k=low;
-        
-        while(i<n1 && j<n2)
-        {
-            if(leftArr[i]<=rightArr[j])
-                arr[k++] = leftArr[i++];
-            else
-                arr[k++] = rightArr[j++];
-        }
-        
-        while(i<n1)
-            arr[k++] = leftArr[i++];
-        while(j<n2)
-            arr[k++] = rightArr[j++];
-    }
-    void mergeSort(vector<int>& arr, int low, int high)
-    {
-        if(low>=high)
-            return;
-        
-        int mid = (low+high)/2;
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid+1, high);
-        merge(arr, low, mid, high);
-    }
+   
     
     vector<int> sortedSquares(vector<int>& nums) {
         
+        int n = nums.size();
         
- 
+    
+        vector<int> leftArr;
+        vector<int> rightArr;
         
-        for(int i=0;i<nums.size();i++)
-            nums[i] = nums[i]*nums[i];
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]<0)
+                leftArr.push_back(nums[i]*nums[i]);
+            else
+                rightArr.push_back(nums[i]*nums[i]);
+        }
         
-        mergeSort(nums, 0, nums.size()-1);
+        
+        int n1 = leftArr.size();
+        int n2 = rightArr.size();
+        
+        int i=n1-1;
+        int j=0;
+        int k=0;
+        
+        while(i>=0 && j<n2)
+            if(leftArr[i]<=rightArr[j])
+                nums[k++] = leftArr[i--];
+            else
+                nums[k++] = rightArr[j++];
+        
+        while(i>=0)
+            nums[k++] = leftArr[i--];
+        while(j<n2)
+            nums[k++] = rightArr[j++];
         
         return nums;
-        
+    
     }
 };
