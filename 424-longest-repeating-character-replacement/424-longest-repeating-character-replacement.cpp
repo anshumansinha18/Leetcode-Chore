cb10bf442afeab8aba4b807a8b90a3891b9da463
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2022-05-15 19:38:54 +0530

    Time: 20 ms (40.55%), Space: 7.1 MB (53.17%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     
-    int calcMax(vector<int> c)
-    {
-        int n = c.size();
-        int maxf = INT_MIN;
-        for(int i=0;i<n;i++)
-        {
-            maxf = max(maxf, c[i]);
-        }
-        return maxf;
-    }
-    
+  
     int characterReplacement(string s, int k) {
         
         int i=0,j=0;
         int n = s.length();
         int res=0;
-        vector<int> m(26,0);
-        
+        unordered_map<int, int> m;
+        int maxf=0;
         while(j<n)
         {
-            m[s[j]-65]++;
-            int maxf = calcMax(m);
+            maxf = max(maxf, ++m[s[j]]);
             int wl = j-i+1;
             if(wl-maxf<=k)
             {
@@ -31,9 +20,9 @@ public:
             }
             else
             {
-                m[s[i]-65]--;
+                m[s[i]]--;
                 i++;
-                m[s[j]-65]--;
+                m[s[j]]--;
             }
         }
         
