e95b47668407fe29d459147ff72d5a15315be3cb
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2022-08-22 20:05:22 +0530

    Time: 18 ms (70.93%), Space: 10.5 MB (59.07%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,19 +1,54 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> m;
+    
+    int binarySearch(vector<int>& arr, int low, int high, int b, int i)
+    {
+        int ans=-1;
         
-        for(int i=0;i<nums.size();i++)
-             m[nums[i]]++;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+           
+            if(arr[mid]==b && mid!=i)
+            {
+                ans = mid;
+                break;
+            }
+            else if(b>arr[mid])
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        
+        return ans;
+        
+    }
+    
+    vector<int> twoSum(vector<int>& nums, int target) {
+    
+        vector<int> temp = nums;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
         
         vector<int> res;
-        for(int i=0;i<nums.size();i++)
+        int ans1, ans2;
+        
+        for(int i=0;i<n;i++)
         {
-            m[nums[i]]--;
-               int c = target-nums[i];
-               if(m[c]>0)
-                   res.push_back(i);
-            m[nums[i]]++;
+           int b = target - nums[i];
+            
+            int x = binarySearch(nums, 0, n-1, b, i);
+            if(x!=-1)
+            {
+                ans1 = i;
+                ans2=x;
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(nums[ans1]==temp[i] || nums[ans2] == temp[i])
+                res.push_back(i);
         }
         
         return res;
