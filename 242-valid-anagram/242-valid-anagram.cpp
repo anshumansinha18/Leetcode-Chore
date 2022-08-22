992affdc6da12d955f8437880d36cc544f197354
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2022-08-22 17:27:49 +0530

    Time: 11 ms (69.56%), Space: 7.2 MB (78.25%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -5,19 +5,17 @@ public:
         
         if(s.size()!=t.size()) return false;
         
-        unordered_map<char, int> m1,m2;
+        unordered_map<char, int> countChars;
         
         for(int i=0;i<s.size();i++)
         {
-            m1[s[i]]++;
-            m2[t[i]]++;
+            countChars[s[i]]++;
+            countChars[t[i]]--;
         }
         
-        for(auto it: m1)
-        {
-            if(m2[it.first]!=it.second)
+        for(auto it: countChars)
+            if(it.second)
                 return false;
-        }
         
         return true;
       
