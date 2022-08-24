4fa20e618cce492be8ca6bcdca49dfe2ad719f1e
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2022-08-24 21:52:11 +0530

    Time: 41 ms (41.71%), Space: 21.3 MB (11.33%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,35 +1,72 @@
 class Solution {
 public:
-     bool isValidSudoku(vector<vector<char>>& board) {
-
-        set<string> s;
+    
+    bool checkRow(vector<vector<char>>& board){
+         
+        for(int i=0;i<9;i++)
+        {
+            unordered_set<int> s;
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j]!='.')
+                if(!s.insert(board[i][j]).second)
+                    return false;
+            }
+        }
+        return true;
+    }
+    
+    bool checkCol(vector<vector<char>>& board){
+         
+        for(int i=0;i<9;i++)
+        {
+            unordered_set<int> s;
+            for(int j=0;j<9;j++)
+            {
+                if(board[j][i]!='.')
+                if(!s.insert(board[j][i]).second)
+                    return false;
+            }
+        }
+        return true;
+    }
+    
+    bool checkBox(vector<vector<char>>& board){
+     
+        
+        vector<unordered_set<char>> box(9);
+        
         for(int i=0;i<9;i++)
         {
             for(int j=0;j<9;j++)
             {
+                int boxNo = i/3*3 + j/3;
+                
                 if(board[i][j]!='.') {
-                    int boxno = i / 3 * 3 + j / 3;
-                    string row = "r", col = "c", box = "b";
-
-                    row += char(i + '0');
-                    row += char(board[i][j]);
-
-                    col += char(j + '0');
-                    col += char(board[i][j]);
-
-                    box += char(boxno + '0');
-                    box += char(board[i][j]);
-
-                    if (s.empty() || (s.find(row) == s.end() && s.find(col) == s.end() && s.find(box) == s.end())) {
-                        s.insert(row);
-                        s.insert(col);
-                        s.insert(box);
-                    } else
+                    auto it = box[boxNo].insert(board[i][j]);
+                    if(!it.second)
                         return false;
-                }
+                    
+                    }
             }
-
         }
-           return true;
+        
+        return true;
+    }
+    
+    bool isValidSudoku(vector<vector<char>>& board) {
+        
+        bool x = checkRow(board);
+        bool y = checkCol(board);
+        bool z = checkBox(board);
+        
+        cout<<x<<endl;
+        cout<<y<<endl;
+        cout<<z<<endl;
+        
+        if(x==true && y == true && z == true)
+            return true;
+        else 
+            return false;
     }
 };
