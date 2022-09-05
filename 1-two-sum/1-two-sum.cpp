9ca782a7159a2f52173e50ab8b52ac5e4e164c1f
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2022-09-05 17:01:09 +0530

    Time: 34 ms (45.52%), Space: 14.5 MB (5.45%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,56 +1,42 @@
 class Solution {
 public:
     
-    int binarySearch(vector<int>& arr, int low, int high, int b, int i)
-    {
-        int ans=-1;
-        
-        while(low<=high)
-        {
-            int mid = (low+high)/2;
-           
-            if(arr[mid]==b && mid!=i)
-            {
-                ans = mid;
-                break;
-            }
-            else if(b>arr[mid])
-                low=mid+1;
-            else
-                high=mid-1;
-        }
-        
-        return ans;
-        
-    }
     
     vector<int> twoSum(vector<int>& nums, int target) {
-    
-        vector<int> temp = nums;
-        sort(nums.begin(), nums.end());
+        
+        unordered_map<int, int> m, index;
+        
         int n = nums.size();
         
-        vector<int> res;
-        int ans1, ans2;
-        
         for(int i=0;i<n;i++)
         {
-           int b = target - nums[i];
-            
-            int x = binarySearch(nums, 0, n-1, b, i);
-            if(x!=-1)
-            {
-                ans1 = i;
-                ans2=x;
-            }
+          m[nums[i]]++;  
+          index[nums[i]] = i;
         }
         
-        for(int i=0;i<n;i++)
-        {
-            if(nums[ans1]==temp[i] || nums[ans2] == temp[i])
-                res.push_back(i);
-        }
         
-        return res;
+        int ans1;
+        int ans2;
+         for(int i=0;i<n;i++)
+         {
+             int a = nums[i];
+             int b = target - a;
+             m[a]--;
+             
+             if(m[b])
+             {
+                 ans1 = i;
+                 ans2 = index[b];
+                 break;
+             }
+             else
+                 m[a]++;
+                 
+         }
+        
+        vector<int> v{ans1, ans2};
+        
+        
+        return v;
     }
 };
