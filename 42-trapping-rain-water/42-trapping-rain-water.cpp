10380da9e10f63a4205ecd540506361f82154ac5
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2022-09-06 19:05:43 +0530

    Time: 37 ms (15.89%), Space: 20.5 MB (25.69%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-     int n = height.size();
-     int max_left = height[0];
-     int max_right = height[n-1];
-
-     int l = 0;
-     int r = n-1;
-
-     int trapWater = 0;
-     
-     vector<int> res(n);
-     while(l<=r)
-     {
-         if(max_left<=max_right)
-         {
-             trapWater += (max_left - height[l])<0?0:(max_left-height[l]);
-             max_left = max(max_left, height[l]);
-             l++;
-         }
-         else
-         {
-             trapWater += (max_right - height[r])<0?0:(max_right-height[r]);
-             max_right = max(max_right, height[r]);
-             r--;
-         }
-     }
-
-     return trapWater;
-     
-}
+       int n = height.size();
+        
+        vector<int> max_left(n);
+        vector<int> max_right(n);
+        
+        for(int i=0;i<n;i++)
+        {
+            if(i==0) max_left[i] = 0;
+            else
+            {
+                if(height[i-1]>max_left[i-1])
+                    max_left[i] = height[i-1];
+                else
+                    max_left[i] = max_left[i-1];
+            }
+        }
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            if(i==n-1) max_right[i]=0;
+            
+            else{
+                if(height[i+1]>max_right[i+1])
+                    max_right[i] = height[i+1];
+                else
+                    max_right[i] = max_right[i+1];
+            }
+        }
+        
+        int res=0;
+        for(int i=0;i<n;i++)
+        {
+            int min_height = min(max_left[i], max_right[i]);
+            
+            int additive = min_height >= height[i] ? min_height-height[i]:0;
+            res = res + additive;
+        }
+        
+        return res;
+    }
 };
