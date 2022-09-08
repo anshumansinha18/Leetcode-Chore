fe686a67ac2156baf78e0b5dfd1a31b8dbde7ca2
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2022-09-09 00:50:49 +0530

    Time: 17 ms (73.73%), Space: 8.3 MB (60.43%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -8,36 +8,23 @@ public:
         int l = 0;
         int r = 0;
         
-        unordered_map<int, int> m;
-        int max_len=1;
-        int len;
-        while(r<n)
+       vector<int> v(256, -1);
+        int max_len = 1;
+        
+    while(r<n)
+    {
+        if(v[s[r]]!=-1)
         {
-            if(m[s[r]]>0)
-            {
-                //rep
-                max_len = max(max_len, len);
-                
-                while(s[l]!=s[r] && l<n)
-                {
-                    m[s[l]]--;
-                    l++;
-                }
-                m[s[l]]--;
-                l++;
-            }
-            else
-            {
-                
-                //no rep
-                m[s[r]]++;
-                r++;
-                len = r-l;
-                
-            }
+            if(v[s[r]]>=l && v[s[r]]<=r)
+                l = v[s[r]]+1;
         }
         
+        v[s[r]] = r;
+        int len = r-l+1;
         max_len = max(max_len, len);
+        r++;
+    }
+        
         return max_len;
     }
 };
