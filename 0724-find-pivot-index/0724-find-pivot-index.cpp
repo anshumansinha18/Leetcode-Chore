68f21e1351f6f260d7550a61600f6b304501e6c4
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-05-17 02:11:36 +0530

    Time: 21 ms (83.66%), Space: 31 MB (91.59%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -4,20 +4,23 @@ public:
         
         int n = nums.size();
         
-        
-        vector<int> leftArr(n);
-        vector<int> rightArr(n);
-        
-        for(int i=0;i<n;i++)
-            leftArr[i] = i==0? nums[i] : leftArr[i-1]+nums[i];
-        
-        for(int i=n-1;i>=0;i--)
-            rightArr[i] = i== n-1 ? nums[i] : rightArr[i+1] + nums[i];
+        int rightSum = 0;
+        int leftSum=0;
         
         for(int i=0;i<n;i++){
-            if(leftArr[i]==rightArr[i])
-                return i;
+            rightSum += nums[i];
         }
+      
+        for(int i=0;i<n;i++){
+            leftSum += nums[i];
+            
+            if(leftSum == rightSum)
+                return i;
+            
+            rightSum -= nums[i];
+        }
+        
         return -1;
+       
     }
 };
