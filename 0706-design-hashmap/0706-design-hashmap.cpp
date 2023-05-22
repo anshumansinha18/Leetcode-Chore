96b793ade2119c64a0ac44be749160d2d6a640e8
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-05-23 03:40:24 +0530

    Time: 127 ms (80.27%), Space: 54.5 MB (62.37%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,9 +1,12 @@
 class HashNode{
-public: int data;
+public:
+    int key;
+    int data;
     HashNode *next;
 
 
-    HashNode(int value){
+    HashNode(int key, int value){
+        this->key = key;
         this->data = value;
         this->next= nullptr;
     }
@@ -18,27 +21,68 @@ class MyHashMap {
     HashNode** arr;
 public:
     MyHashMap() {
-        arr = new HashNode*[1000001];
-        for(int i=0;i<1000001;i++){
+        arr = new HashNode*[10007];
+        for(int i=0;i<10007;i++){
             arr[i] = NULL;
         }
     }
 
     void put(int key, int value) {
-        HashNode *node = new HashNode(value);
+        HashNode *node = new HashNode(key, value);
+        int idx = key%10007;
 
-        arr[key] = node;
+        if(arr[idx]== nullptr)
+            arr[idx]= node;
+        else{
+            HashNode *temp = arr[idx];
+            while(temp->next!= nullptr)
+                temp = temp->next;
+            temp->next = node;
+        }
     }
 
     int get(int key) {
+        int idx = key%10007;
 
-       if(arr[key]==NULL)
-           return -1;
-       else return arr[key]->data;
+        if(arr[idx]== nullptr)
+            return -1;
+        else{
+            HashNode *temp = arr[idx];
+            int result=-1;
+            while(temp){
+                if(temp->key==key){
+                    result=temp->data;
+                }
+                temp=temp->next;
+            }
+            return result;
+        }
     }
 
     void remove(int key) {
-  
-      arr[key] = nullptr;
+      //first element removal:
+      int idx = key%10007;
+     HashNode *curr = arr[idx];
+     HashNode *prev = arr[idx];
+
+     while(curr) {
+         if (curr == arr[idx]) {
+             if (curr->key == key) {
+                 arr[idx] = curr->next;
+                 curr = arr[idx];
+                 prev = curr;
+             } else
+                 curr = curr->next;
+         } else {
+             if (curr->key == key) {
+                 prev->next = curr->next;
+                 curr = curr->next;
+             } else {
+                 prev = curr;
+                 curr = curr->next;
+             }
+         }
+     }
+
     }
 };
