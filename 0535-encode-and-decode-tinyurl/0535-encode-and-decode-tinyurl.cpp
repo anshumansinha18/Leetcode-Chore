1897d882e2a88a9a0c45cf13dc5fcd12b3bb0d42
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-05-25 02:21:25 +0530

    Time: 9 ms (12.84%), Space: 7.8 MB (7.35%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,66 @@
 class Solution {
-public:
 
+    private:
+       unordered_map<char, string> m;
+       string fixedUrl = "http://tinyurl.com/";
+public:
+     int randomIndex(){
+         int randIndex = rand() % 3;
+         return randIndex;
+     }
+     string randomString(){
+         vector<pair<int, int>> v;
+
+         v.push_back(make_pair(48, 57));
+         v.push_back(make_pair(65, 90));
+         v.push_back(make_pair(97, 122));
+
+
+         set<char> s;
+
+         while(s.size()<6){
+             int x = randomIndex();
+             int lb = v[x].first;
+             int ub = v[x].second;
+             char randChar = (char)(rand()%(ub-lb+1) + lb);
+             s.insert(randChar);
+         }
+         string randString = "";
+         for(auto it: s)
+             randString+=it;
+         return randString;
+     }
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl;
+
+         int n = longUrl.size();
+         string res="";
+         string shortUrl = randomString();
+         int j=0;
+
+         for(int i=0;i<n;i++){
+             res+=longUrl[i];
+
+             if(i%6==5 && j<shortUrl.size()) {
+                 m[shortUrl[j++]] = res;
+                 res = "";
+             }
+         }
+         m[shortUrl[--j]]+=res;
+
+        return fixedUrl+shortUrl;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+
+        int n = shortUrl.size();
+        string res = "";
+        for(int i=fixedUrl.size();i<n;i++){
+            res+=m[shortUrl[i]];
+        }
+
+        return res;
     }
 };
 
