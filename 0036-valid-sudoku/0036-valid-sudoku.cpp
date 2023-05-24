46a5efbe4042bfd97a93885937c7ad89b8ceac87
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-05-24 17:49:32 +0530

    Time: 28 ms (40.92%), Space: 21.7 MB (6.80%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -10,15 +10,13 @@ public:
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[i].size();j++){
                 if(board[i][j]!='.'){
-                    if(row[i][board[i][j]]) return false;
-                    else row[i][board[i][j]]++;
-                    
-                    if(column[j][board[i][j]]) return false;
-                    else column[j][board[i][j]]++;
-                    
                     int boxId = i/3*3 + j/3;
-                    if(box[boxId][board[i][j]]) return false;
-                    else box[boxId][board[i][j]]++;
+                    if(row[i][board[i][j]] || column[j][board[i][j]] || box[boxId][board[i][j]])                              return false;
+                    else{
+                        row[i][board[i][j]]++;
+                        column[j][board[i][j]]++;
+                        box[boxId][board[i][j]]++;
+                    }
                 }
             }
         }
