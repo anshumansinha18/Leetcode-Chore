65af7f9383051180bbc04f2497fdd25172f19edf
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-05-25 00:07:49 +0530

    Time: 892 ms (11.31%), Space: 69.2 MB (5.14%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -3,33 +3,23 @@ public:
     int longestConsecutive(vector<int>& nums) {
         
         unordered_map<int, int> m;
+        int maxL=0;
         int n = nums.size();
-        
         for(int i=0;i<n;i++){
             m[nums[i]]++;
         }
         
         
-        int maxL = 0;
         for(int i=0;i<n;i++){
             int length=1;
             
-            if(m[nums[i]]){
-                m[nums[i]]--;
+            if(!m[nums[i]-1]){
                 int x = nums[i];
-                while(m[--x]){
+                while(m[++x])
                     length++;
-                    m[x]--;
-                }
-                
-                x=nums[i];
-                while(m[++x]){
-                    length++;
-                    m[x]--;
-                }
-                
-                maxL = max(maxL, length);
             }
+            
+            maxL = max(maxL, length);
         }
         
         return maxL;
