528c12696edbb90c9184abb363c8a8ff430ac5f7
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-06-13 16:43:45 +0530

    Time: 54 ms (14.80%), Space: 20.9 MB (7.84%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    void compare(vector<vector<int>> &res, vector<int> a, vector<int> b){
+        
+        if(a[1]<b[0] || a[0]>b[1]){
+            res.push_back(b);
+        }else{
+            res[res.size()-1][0]=min(a[0], b[0]);
+            res[res.size()-1][1]=max(a[1],b[1]);
+        }
+    }
+    
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        
+        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end());
+        res.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            
+            compare(res, res[res.size()-1], intervals[i]);
+        }
+        
+        return res;
+    }
+};
