d4709c8a6165b8b9d399e31da01088c5e3caf53d
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-06-15 03:16:09 +0530

    Time: 28 ms (5.14%), Space: 15.6 MB (35.42%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,39 +1,17 @@
 class Solution {
 public:
-    
-    int binarySearch(vector<int> &nums,int x, int i){
-        
-        int low=0;
-        int high=nums.size()-1;
-        int n = nums.size();
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            
-            if(nums[mid]==x && mid!=i){
-                return mid;
-            }else if(mid==i && mid<n-1 && nums[mid+1]==x)
-                return mid+1;
-            else if(x>nums[mid]) low=mid+1;
-            else high=mid-1;
-        }
-        
-        return -1;
-    }
     vector<int> twoSum(vector<int>& numbers, int target) {
         
-        int n = numbers.size();
+        int low=0;
+        int high = numbers.size()-1;
         
-        vector<int> res(2);
-        for(int i=0;i<n;i++){
-            int x = target-numbers[i];
-            
-            int index = binarySearch(numbers,x, i);
-            if(index!=-1){
-                res = {i+1, index+1};
-                break;
-            }
+        while(low<high){
+            if(numbers[low]+numbers[high]==target) break;
+            else if(numbers[low]+numbers[high]>target) high--;
+            else low++;
         }
         
+        vector<int> res = {low+1, high+1};
         return res;
     }
 };
