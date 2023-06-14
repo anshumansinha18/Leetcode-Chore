f1d3ed9bbb629fb00001b2ee57f49bc0e0490ba9
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-06-15 01:12:31 +0530

    Time: 601 ms (70.05%), Space: 92.7 MB (30.48%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        
+        vector<unordered_map<string, int>> vec(26);
+        
+        
+        for(int i=0;i<ideas.size();i++)
+            vec[ideas[i][0]-97][ideas[i].substr(1)]++;
+        
+        
+//         for(int i=0;i<vec.size();i++){
+//             for(int j=0;j<vec[i].size();j++){
+//                 cout<<vec[i][j]<<" ";
+//             }
+//             cout<<endl;
+//         }
+        
+        long long count=0;
+        
+        for(int i=0;i<25;i++){
+            for(int j=i+1;j<26;j++){
+               long long x=0;
+                
+                for(auto &it: vec[i]){
+                    if(vec[j].find(it.first)!=vec[j].end())
+                       x++;
+                }
+                count+=2*(vec[i].size()-x)*(vec[j].size()-x);
+            }
+            
+            
+        }
+        
+        return count;
+    }
+};
