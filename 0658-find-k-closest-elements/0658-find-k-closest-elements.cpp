5ca97dccd75ce6efed82a58bd5844deaef08d996
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-06-29 02:19:05 +0530

    Time: 57 ms (48.12%), Space: 31.7 MB (58.79%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,78 +1,65 @@
 class Solution {
 public:
     
-    int findIndex(vector<int>& arr, int x){
-       
-         int low=0;
+    int binarySearch(vector<int>& arr, int x){
+        int low=0;
         int high=arr.size()-1;
-        int n = arr.size();
-        int index=0;
-        int mid;
+        int mid=-1;
         while(low<=high){
-             mid = low + (high-low)/2;
+             mid = low+(high-low)/2;
             
-            if(arr[mid] == x){
-                return mid;
-            }
-               
-            else if(x>arr[mid]){
-                low=mid+1;
-            }
-            else
-                high=mid-1;
+                
+            if(arr[mid]==x) return mid;
+            else if(x>arr[mid]) low=mid+1;
+            else high=mid-1;
         }
         
-        cout<<low<<high<<endl;
-        
-        if(low>=0 && low<=n-1 && high>=0 && high<=n-1){
-            int a = arr[low];
-            int b = arr[high];
-             if((abs(a-x) < abs(b-x)) || (abs(a-x) == abs(b-x) && a<b))
-                 return low;
-            else return high;
-        }else if(high>=0 && high<=n-1){
-            return high;
-        }else return low;
-        
+        return mid;
     }
-    
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         
-        int n = arr.size();
-         
-        int index = findIndex(arr, x);
-        int i=index;
-        int j=index;
+        int index = binarySearch(arr, x);
+        cout<<index<<endl;
+        int low;
+        int high;
         
-        
+        if(index<=(arr.size()-1)/2){
+            low=index;
+            high=index+1;
+        }else{
+            low=index-1;
+            high=index;
+        }
+        cout<<low<<" "<<high<<endl;
         vector<int> res;
-        
-        while(res.size()<k){
-            if(i==j){
-                res.push_back(arr[i]);
-                i--;
-                j++;
-            }else if(i<0 && j<n){
-                res.push_back(arr[j]);
-                j++;
-            }else if(j>=n && i>=0){
-                res.push_back(arr[i]);
-                i--;
-            }else if(i>=0 && j<n){
-                int a=arr[i];
-                int b=arr[j];
-                if((abs(a-x) < abs(b-x)) || (abs(a-x) == abs(b-x) && a<b)){
-                    res.push_back(a);
-                    i--;
-                }else{
-                    res.push_back(b);
-                    j++;
-                } 
+        while(low>=0 && high<=arr.size()-1 && k>0){
+      
+            int a = arr[low];
+            int b =arr[high];
+            
+            if(abs(a-x)<abs(b-x) || (abs(a-x)==abs(b-x) && a<b)){
+                res.push_back(a);
+                low--;
+            }else{
+                res.push_back(b);
+                high++;
             }
+                k--;
         }
         
-        sort(res.begin(), res.end());
-         return res;
+        while(low>=0 && k>0){
+           
+            res.push_back(arr[low]);
+            low--;
+            k--;
+        }
         
+        while(high<=arr.size()-1 && k>0){
+            res.push_back(arr[high]);
+            high++;
+            k--;
+        }
+        sort(res.begin(), res.end());
+        return res;
     }
 };
