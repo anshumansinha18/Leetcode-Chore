bd7a3f62165fbb844105c22ee3f0bdc9895443ea
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-07-06 00:59:43 +0530

    Time: 899 ms (5.02%), Space: 96.7 MB (6.09%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -2,29 +2,21 @@ class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         
+        map<int, double> m;
         int n = position.size();
-        
-        unordered_map<int, int> m;
-        
-        for(int i=0;i<n;i++)
-            m[position[i]] = speed[i];
-        sort(position.begin(), position.end());
-        
-        stack<int> st;
-        
-        double time;
-        
-        for(int i=n-1;i>=0;i--)   
-        {
-            st.push(position[i]);
-            double t = (target - position[i])/(double)m[position[i]];
-           
-                if(time>=t)
-                    st.pop();
-                else
-                    time = t;        
+        for(int i=0;i<n;i++){
+            m[position[i]] = (target-position[i])/(double)speed[i];
+        }
+        stack<double> st;
+        for(auto it: m){
+            cout<<it.second<<endl;
+            while(!st.empty() && it.second>=st.top())
+                st.pop();
+            
+            st.push(it.second);
         }
         
         return st.size();
+        
     }
 };
