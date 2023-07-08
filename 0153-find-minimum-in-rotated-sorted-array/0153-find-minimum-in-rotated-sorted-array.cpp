b1c1fced32d10a85c5a8c6286c23d5af3ee8f5b3
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-07-09 05:24:49 +0530

    Time: 3 ms (78.78%), Space: 10.2 MB (47.98%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,7 +4,8 @@ public:
     int binarySearch(vector<int>& nums){
         int low=0;
         int high=nums.size()-1;
-        while(low<=high){
+        while(low<high){
+            cout<<"s";
             if(nums[low]<nums[high]) return low;
             int mid = low+(high-low)/2;
             
@@ -14,25 +15,13 @@ public:
                 high=mid;
             
         }
+        
         return low;
     }
     
     int findMin(vector<int>& nums) {
-       int start=0,end=nums.size()-1;
         
-        while (start<end) {
-            if (nums[start]<nums[end])
-                return nums[start];
-            
-            int mid = (start+end)/2;
-            
-            if (nums[mid]>=nums[start]) {
-                start = mid+1;
-            } else {
-                end = mid;
-            }
-        }
-        
-        return nums[start];
+        int mid = binarySearch(nums);
+        return nums[mid];
     }
 };
