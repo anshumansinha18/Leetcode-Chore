991e46402b4154f551763ad5c4740e70b7559937
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-07-09 04:28:35 +0530

    Time: 229 ms (81.76%), Space: 7 MB (33.06%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
+    
     bool checkPal(string s){
-    int start=0;
-    int end = s.size()-1;
-    while(start<end){
-        if(s[start]!=s[end])
-            return false;
-        start++;
-        end--;
+        int start=0;
+        int end = s.size()-1;
+        
+        while(start<end){
+            if(s[start]!=s[end]) return false;
+            start++;
+            end--;
+        }
+        
+        return true;
     }
-
-    return true;
-}
-
-int maxProduct(string s) {
-
-    vector<pair<int, int>> vec;
-     int n= s.size();
-     for(int i=1;i<(1<<n);i++){
-         string sub = "";
-
-         for(int j=0;j<=n;j++){
-             if(i & (1<<j)){
-                 sub+=s[j];
-             }
-         }
-
-         if(checkPal(sub)) {
-             vec.push_back(make_pair(i, sub.size()));
-         }
-
-     }
-    int res = 0;
-    for(int i=0;i<vec.size();i++){
-        for(int j=i+1;j<vec.size();j++){
-            if(!(vec[i].first & vec[j].first)){
-                res = max(res, vec[i].second*vec[j].second);
+    int maxProduct(string s) {
+        
+        int n = s.size();
+        vector<pair<int, int>> vec;
+        for(int i=1;i<(1<<n);i++){
+            string check="";
+            for(int j=0;j<n;j++){
+                int x = 1<<j;
+                if((i&x)!=0){
+                    check+=s[j];
+                }
+            }
+            if(checkPal(check)){ 
+  
+                vec.push_back(make_pair(i, check.size()));}
+        }
+        
+        int maxProd=0;
+        for(int i=0;i<vec.size()-1;i++){
+            for(int j=i+1;j<vec.size();j++){
+            
+                if((vec[i].first&vec[j].first)==0){
+                    maxProd = max(maxProd, vec[i].second*vec[j].second);
+                }
             }
         }
+        
+        return maxProd;
     }
-    return res;
-
-
-}
 };
