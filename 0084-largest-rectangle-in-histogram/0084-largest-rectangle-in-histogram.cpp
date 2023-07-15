f85131ba07b554c170631d45801d3da790f909bf
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-07-16 01:39:57 +0530

    Time: 220 ms (52.82%), Space: 85.1 MB (32.73%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,49 +1,56 @@
 class Solution {
 public:
+    
+    vector<int> generateNSELeft(vector<int> &nums){
+        stack<int> st;
+        int n = nums.size();
+        vector<int> res(n, -1);
+        
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[i]<nums[st.top()]){
+                res[st.top()]=i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        return res;
+    }
+    
+    vector<int> generateNSERight(vector<int> &nums){
+        stack<int> st;
+        
+        int n = nums.size();
+        vector<int> res(n, n);
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[i]<nums[st.top()]){
+                res[st.top()]=i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        
+        return res;
+    }
     int largestRectangleArea(vector<int>& heights) {
+        
+        vector<int> nseR = generateNSERight(heights);
+        vector<int> nseL = generateNSELeft(heights);
         
         int n = heights.size();
         
-        vector<int> rb(n, n);
-        vector<int> lb(n, -1);
-        
-        stack<int> st;
-        
-        //right boundary code
-        for(int i=0;i<n;i++)
-        {
-            while(!st.empty() && heights[i]<heights[st.top()])
-            {
-                rb[st.top()] = i;
-                st.pop();
-            }
-            st.push(i);
+        // for(int i=0;i<n;i++){
+        //     cout<<nseR[i]<<" ";
+        // }
+        // cout<<endl;
+        // for(int i=0;i<n;i++){
+        //     cout<<nseL[i]<<" ";
+        // }
+        // return 0;
+        int maxArea=0;
+        for(int i=0;i<n;i++){
+            maxArea = max((nseR[i]-nseL[i]-1)*heights[i], maxArea);
         }
-        while(!st.empty()) st.pop();
-      
-        //left boundary code
         
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!st.empty() && heights[i]<heights[st.top()])
-            {
-                lb[st.top()] = i;
-                st.pop();
-            }
-            st.push(i);
-        }
-        while(!st.empty()) st.pop();
-       
-        
-        
-        int maxArea = INT_MIN;
-        for(int i=0;i<n;i++)
-        {
-            int area = (rb[i]-lb[i] - 1) * heights[i];
-            if(area>maxArea) maxArea = area;
-            
-        }
         return maxArea;
-        
     }
 };
