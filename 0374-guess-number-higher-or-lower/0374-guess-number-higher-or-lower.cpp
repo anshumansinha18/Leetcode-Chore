380cdfeffc31f941fc6f3874d9acb6a641fa59e9
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-07-17 02:32:46 +0530

    Time: 3 ms (44.56%), Space: 5.9 MB (42.66%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -13,17 +13,16 @@ public:
         int low=1;
         int high=n;
         
-        while(low<=high){
+        while(low<high){
             int mid = low+(high-low)/2;
             
-            if(guess(mid)==0)
-                return mid;
-            else if(guess(mid)==1){
+            if(guess(mid)==1){
                 low=mid+1;
             }else
-                high=mid-1;
+                high=mid;
+            
         }
         
-        return -1;
+        return low;
     }
 };
