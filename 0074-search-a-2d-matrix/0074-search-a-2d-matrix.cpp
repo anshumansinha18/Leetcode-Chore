836ce8689f0cc8403f89c52260dac9fc0d575e60
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-07-21 19:47:30 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (91.41%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,48 +1,39 @@
 class Solution {
 public:
-    
-    int checkRow(vector<vector<int>>matrix, int target){
-        
-        int i=0;
-        int n = matrix[0].size();
-        int index = 0;
-        while(i<matrix.size()){
-            
-            if(target>matrix[i][n-1])
-                i++;
-            else{
-                index=i;
-                break;
-            }
-        }
-        
-        return index;
-    }
-    
-    int binarySearch(vector<int>& arr, int l, int r, int data){
-        if(l>r)
-            return -1;
-        
-        int mid = l + (r-l)/2;
-        
-        if(arr[mid]==data)
-            return mid;
-        else if(data>arr[mid])
-            return binarySearch(arr, mid+1, r, data);
-        else
-            return binarySearch(arr, l, mid-1, data);
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
+        int low=0;
+        int high=matrix.size()-1;
         
-        int row = checkRow(matrix, target);
+        while(low<high){
+            int mid = low+(high-low)/2;
+            
+            if(target>matrix[mid][0])
+                low=mid+1;
+            else
+                high=mid;
+        }
         
-        vector<int> vec = matrix[row];
+        if(matrix[low][0]==target) return true;
+        else if(target<matrix[low][0]) low--;
         
-        int res = binarySearch(vec, 0, vec.size()-1, target);
-      
-        if(res!=-1) return true;
-        else return false;
-    
+        if(low<0) return false;
+        
+        
+        int l=0;
+        int h=matrix[low].size()-1;
+        
+        while(l<h){
+            int mid = l+(h-l)/2;
+            
+            if(target>matrix[low][mid])
+                l=mid+1;
+            else
+                h=mid;
+        }
+        
+        return matrix[low][l]==target?true:false;
+        
+        
     }
 };
