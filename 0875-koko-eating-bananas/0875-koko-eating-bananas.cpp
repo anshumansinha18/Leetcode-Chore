997ab0407feef2369e7854ef5f4b1b6adf64b61b
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-07-21 20:09:25 +0530

    Time: 84 ms (11.52%), Space: 18.9 MB (37.47%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
-    
-    int binarySearch(vector<int>& piles, int low, int high, int h){
+    bool condition(vector<int> &nums, int mid, int h){
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%mid==0) count+=nums[i]/mid;
+            else count+= nums[i]/mid +1;
+        }
+        return count>h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
         
-        int ans = INT_MAX;
-        int mid;
-        while(low<=high)
-        {
-            mid = low+(high-low)/2;
-            
-            long long int sum_hours = 0;
-            
-            for(int i=0;i<piles.size();i++)
-                sum_hours += ceil(piles[i]*1.0/mid);
-            
-            
-            if(sum_hours<=h){
-                high = mid-1;
-                ans = min(ans, mid);
-            }
-            else{
-                low = mid+1;
-            }
-        
+        int maxVal=0;
+        int n = piles.size();
+        for(int i=0;i<n;i++){
+            maxVal = max(maxVal, piles[i]);
         }
         
-        return ans;
-    }
-    
-    int minEatingSpeed(vector<int>& piles, int h) {
-         
-        int max_el = INT_MIN;
+        int low=1;
+        int high=maxVal;
         
-        int n = piles.size();
-        
-        for(int i=0;i<n;i++)
-           max_el = max(max_el, piles[i]);
-    
-        
-        return binarySearch(piles, 1, max_el, h);
+        while(low<high){
+            int mid = (low+(high-low)/2);
+            
+            if(condition(piles, mid, h)){
+                low=mid+1;
+            }else
+                high=mid;
+        }
+        return low;
     }
 };
