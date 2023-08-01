cbb3c6a95e47213189c83deb6f4c54886d942fed
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-08-01 08:11:01 +0530

    Time: 662 ms (10.61%), Space: 180 MB (75.05%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -1,3 +1,4 @@
+
 class LFUCache {
     map<int,  list<pair<int, int>>> m;
     unordered_map<int, pair<int, int>> mp;
@@ -12,34 +13,17 @@ public:
     }
 
     int get(int key) {
-        if(mp.find(key)==mp.end()){
-            return -1;
-        }
-
-        int result;
-        for(auto it= m[mp[key].first].begin();it!=m[mp[key].first].end();it++){
-            if((*it).first==key)
-                result=(*it).second;
-        }
-
+        if(mp.find(key)==mp.end()) return -1;
+        
+        int result=mp[key].second;
         makeMostFrequent(key, result, result);
         return result;
-
-
-
     }
-
-
-
+    
     void put(int key, int value) {
 
         if(mp.find(key)!=mp.end()){
-            int result;
-            for(auto it= m[mp[key].first].begin();it!=m[mp[key].first].end();it++){
-                if((*it).first==key)
-                    result=(*it).second;
-            }
-
+            int result=mp[key].second;
             makeMostFrequent(key, result, value);
         }else curr_size++;
 
@@ -55,6 +39,7 @@ public:
         if(mp.find(key)==mp.end()) {
             m[1].push_front(make_pair(key, value));
             mp[key].first = 1;
+            mp[key].second=value;
         }
 
     }
@@ -62,6 +47,7 @@ public:
         m[mp[key].first].remove(make_pair(key, value));
         if(m[mp[key].first].size()==0) m.erase(mp[key].first);
         mp[key].first++;
+        mp[key].second=updatedValue;
         m[mp[key].first].push_front(make_pair(key, updatedValue));
     }
 };
