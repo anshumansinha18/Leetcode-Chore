457b0c11600a02ab20a4bdd29f740ca720948f4a
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-08-12 04:13:06 +0530

    Time: 1183 ms (27.71%), Space: 90.4 MB (29.34%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.cpp
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -2,28 +2,34 @@ class Solution {
 public:
     long long distinctNames(vector<string>& ideas) {
         
-        vector<unordered_map<string, int>> vec(26);
+        vector<map<string, int>> vec(26);
         
-        
-        for(int i=0;i<ideas.size();i++)
-            vec[ideas[i][0]-97][ideas[i].substr(1)]++;
-
-        long long count=0;
-        
-        for(int i=0;i<25;i++){
-            for(int j=i+1;j<26;j++){
-               long long x=0;
-                
-                for(auto &it: vec[i]){
-                    if(vec[j].find(it.first)!=vec[j].end())
-                       x++;
-                }
-                count+=2*(vec[i].size()-x)*(vec[j].size()-x);
-            }
+        for(int i=0;i<ideas.size();i++){
+            string s="";
+            for(int j=1;j<ideas[i].size();j++)
+                s+=ideas[i][j];
             
-            
+            vec[ideas[i][0]-'a'][s]++;
         }
         
-        return count;
+        // for(int i=0;i<26;i++){
+        //     cout<<char(i+'a')<<" ";
+        //     for(auto it: vec[i])
+        //         cout<<it.first<<" ";
+        //     cout<<endl;
+        // }
+        long long answer=0;
+        for(int i=0;i<26;i++){
+            for(int j=i+1;j<26;j++){
+                if(vec[i].size() && vec[j].size()){
+                  int count=0;
+                 for(auto it: vec[i]){
+                    if(vec[j].find(it.first)!=vec[j].end()) count++;
+                 }
+                answer += 2*(vec[i].size()-count)*(vec[j].size()-count);
+                }
+            }
+        }
+        return answer;
     }
 };
