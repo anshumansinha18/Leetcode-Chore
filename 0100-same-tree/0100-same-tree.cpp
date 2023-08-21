90fb42a6313490884c34b146e75498485931d0ca
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-08-22 05:11:50 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (68.74%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,25 +11,8 @@
  */
 class Solution {
 public:
-    bool answer=true;
-    void inorder(TreeNode* p, TreeNode* q){
-        if(p && q){
-            inorder(p->left, q->left);
-            if(p->val != q->val){
-                answer=false;
-            }
-            inorder(p->right, q->right);
-        }
-        else if(p && !q){
-            answer=false;
-            return;
-        }else if(q && !p){
-            answer=false;
-            return;
-        }
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-         inorder(p, q);
-        return answer;
+        if(p == nullptr || q==nullptr) return p==q;
+        return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
     }
 };
