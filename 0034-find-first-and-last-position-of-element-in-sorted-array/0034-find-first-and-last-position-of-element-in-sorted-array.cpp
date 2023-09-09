f9702d8948267abfe5484bf90adf8a799bd9c942
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-09-10 04:39:32 +0530

    Time: 7 ms (59.46%), Space: 13.6 MB (36.31%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,56 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
         int low=0;
-        int high=n-1;
-        vector<int> res(2, -1);
-        if(n==0) return res;
-        while(low<high){
-            int mid=low+(high-low)/2;
-            
-            if(target>nums[mid])
-                low=mid+1;
-            else
-                high=mid;
+        int high=nums.size()-1;
+        int n =nums.size();
+
+        vector<int> res = {-1, -1};
+
+        if(nums.size()==1 && nums[0]==target){
+            res = {0,0};
+            return res;
         }
-        
-        if(nums[low]!=target) return res;
-        else res[0]=low;
-        
-        high=n-1;
-        
-        while(low<high){
-            int mid=ceil(low+(high-(double)low)/2);
+
+        int minIndex = n;
+        int maxIndex=-1;
+        while(low<=high){
             
-            if(target>=nums[mid])
-                low=mid;
-            else
+            int mid = low+(high-low)/2;
+
+            if(nums[mid]==target){
+                minIndex = min(minIndex, mid);
                 high=mid-1;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
-        
-        res[1]=high;
-        return res;
-        
+        cout<<minIndex;
+
+        if(minIndex>=n) return {-1, -1};
+
+        low=0;
+        high=n-1;
+        while(low<=high){
+            
+            int mid = low+(high-low)/2;
+
+            if(nums[mid]==target){
+                maxIndex = max(maxIndex, mid);
+                low=mid+1;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        cout<<" "<<maxIndex;
+        return {minIndex, maxIndex};
     }
 };
