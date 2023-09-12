906704a7c5885dd6bb4fd80f0298065ad704d3c0
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-09-12 12:47:16 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (12.43%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -10,12 +10,9 @@ public:
         int n = s.size();
         
         for(int i=0;i<n;i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='[' || st.empty())
-                st.push(s[i]);
-            else{
-                if(st.top()==m[s[i]]) st.pop();
-                else st.push(s[i]);
-            }
+            if(!st.empty() && st.top()==m[s[i]])
+               st.pop();
+            else st.push(s[i]);
         }
         
         return st.empty();
