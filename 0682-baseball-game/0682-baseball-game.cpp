33c2c9e95c06219ee0348e8736ae6db709de870c
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-11-20 19:24:35 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (15.53%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
+    bool isOperation(string s){
+        return s=="C" || s=="D" || s=="+";
+    }
+    
+    void performOperation(stack<string>& st, string op){
+        if(op=="+"){
+            int a = stoi(st.top());
+            st.pop();
+            int b = stoi(st.top());
+            st.push(to_string(a));
+            st.push(to_string(a+b));
+        }
+        else if(op=="D"){
+            int a = stoi(st.top());
+            st.push(to_string(a*2));
+        }else if(op=="C"){
+            st.pop();
+        }
+    }
     int calPoints(vector<string>& operations) {
+        stack<string> st;
         
-        int n = operations.size();
-        stack<int> st;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            if(operations[i]=="+"){
-                int x=st.top();
-                st.pop();
-                int y = st.top();
-                st.push(x);
-                st.push(x+y);
-            }else if(operations[i]=="D"){
-                int x = 2*st.top();
-                st.push(x);
-            }else if(operations[i]=="C"){
-                st.pop();
+        for(int i=0;i<operations.size();i++){
+            if(isOperation(operations[i])){
+                performOperation(st, operations[i]);
             }else{
-                st.push(stoi(operations[i]));
+                st.push(operations[i]);
             }
         }
-        
+        int res=0;
         while(!st.empty()){
-            sum+=st.top();
+            res=res+stoi(st.top());
             st.pop();
         }
         
-        return sum;
+        return res;
+        
+        
     }
 };
