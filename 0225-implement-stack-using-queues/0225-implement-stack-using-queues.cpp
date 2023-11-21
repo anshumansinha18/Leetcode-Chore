4d6456162e8de8403ba7d3785edec3fd2a9f5fd0
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-11-22 03:27:27 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (99.12%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,46 +1,31 @@
 class MyStack {
-    queue<int> q1;
-    queue<int> q2;
+    queue<int> q;
 public:
     MyStack() {
         
     }
     
     void push(int x) {
-        if(q1.empty() && q2.empty()) q1.push(x);
-        else if(q1.empty()) q2.push(x);
-        else if(q2.empty()) q1.push(x);
-    }
-    
-    int pop() {
-        if(q1.empty()){
-            while(q2.size()!=1){
-                int x = q2.front();
-                q2.pop();
-                q1.push(x);
-            }
-            int res = q2.front();
-            q2.pop();
-            return res;
-        }else{
-            while(q1.size()!=1){
-                int x = q1.front();
-                q1.pop();
-                q2.push(x);
-            }
-            int res = q1.front();
-            q1.pop();
-            return res;
+        q.push(x);
+        for(int i=1;i<q.size();i++){
+            int y = q.front();
+            q.pop();
+            q.push(y);
         }
     }
     
+    int pop() {
+        int res = q.front();
+        q.pop();
+        return res;
+    }
+    
     int top() {
-        if(q1.empty()) return q2.back();
-        else return q1.back();
+        return q.front();
     }
     
     bool empty() {
-        return (q1.empty() && q2.empty()) ;
+        return q.empty();
     }
 };
 
