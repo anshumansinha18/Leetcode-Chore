db7f875fc3cb9c2ef27464f3c07014f170c629d3
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-12-20 15:27:23 +0530

    Time: 126 ms (20.94%), Space: 72 MB (16.02%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,8 +4,11 @@ public:
         unordered_map<int, int> m;
         
         for(int i=0;i<nums.size();i++){
-            m[nums[i]]++;
-            if(m[nums[i]]>1) return true;
+            m[nums[i]]++;    
+        }
+        
+        for(auto it: m){
+            if(it.second>1) return true; 
         }
         return false;
     }
