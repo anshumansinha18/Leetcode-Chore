86b953c541eaa119f706885a3a6a303b8b88fdff
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-12-21 01:16:54 +0530

    Time: 3 ms (42.76%), Space: 6.9 MB (16.69%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-bool wordPattern(string pattern, string s) {
-
-    unordered_map<string, char> m1;
-    unordered_map<char, string> m2;
-    s+=" ";
-    int n2 = s.size();
-    int n1 = pattern.size();
-    int j=0;
-    string res="";
-    for(int i=0;i<n2;i++){
-        if(s[i]==' '){
-            if((m1[res]!=0 && m1[res]!=pattern[j]) || (m2[pattern[j]]!="" && m2[pattern[j]]!=res))
-                return false;
-            m1[res]=pattern[j];
-            m2[pattern[j++]] = res;
-            res="";
-        }else{
-            res+=s[i];
+    bool wordPattern(string pattern, string s) {
+         s+=' ';
+        int n1=pattern.size();
+        int n2=s.size();
+        unordered_map<char, string> m1;
+        unordered_map<string, char> m2;
+        string res="";
+       
+        int j=0;
+        for(int i=0;i<n2;i++){
+            if(s[i]==' '){
+              
+               if((m1.find(pattern[j])!=m1.end() && m1[pattern[j]]!=res) || (m2.find(res)!=m2.end() && m2[res]!=pattern[j])){
+                   return false;
+               }
+                   m1[pattern[j]]=res;
+                   m2[res]=pattern[j];
+                   j++;
+                   
+             
+                res="";
+            }else{
+                res+=s[i];
+            }
         }
+        return !(j<n1);
     }
-    
-    if(j<n1)
-        return false;
-    return true;
-}
 };
