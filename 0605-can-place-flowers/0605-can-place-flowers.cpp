92c57bb5a1459b4e9dcaba120e5c532f66a0d05c
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-12-20 21:12:48 +0530

    Time: 14 ms (48.97%), Space: 20.7 MB (61.62%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -3,18 +3,14 @@ public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         flowerbed.insert(flowerbed.begin(), 0);
         flowerbed.push_back(0);
+        int count=0;
         
-        int size=flowerbed.size();
-        for(int i=1;i<size-1;i++){
-            
-            if(flowerbed[i-1] + flowerbed[i]+flowerbed[i+1]==0)
-            {
+        for(int i=1;i<flowerbed.size()-1;i++){
+            if(flowerbed[i-1]+flowerbed[i]+flowerbed[i+1]==0){
                 flowerbed[i]=1;
-                n--;
+                count++;
             }
         }
-        
-        return n<=0;
-        
+        return count>=n;
     }
 };
