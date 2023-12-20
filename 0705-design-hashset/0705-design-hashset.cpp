a85d21255a6dcba1911c1a57d5e87228ba12dedb
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-12-21 01:25:29 +0530

    Time: 509 ms (5.02%), Space: 39.6 MB (97.96%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,24 +1,32 @@
 class MyHashSet {
-    private:
-       unordered_map<int, int> m;
-public:
+    
+    public:
+    vector<int> arr;
     MyHashSet() {
         
     }
     
     void add(int key) {
-        m[key]++;
+        if(!contains(key)) arr.push_back(key);
     }
     
     void remove(int key) {
-        if(m.find(key)!=m.end())
-            m.erase(key);
+        if(contains(key)){
+            for(int i=0;i<arr.size();i++){
+                if(key==arr[i]){
+                    swap(arr[i], arr[arr.size()-1]);
+                    break;
+                }
+            }
+            arr.pop_back();
+        }
     }
     
     bool contains(int key) {
-        if(m.find(key)==m.end())
-            return false;
-        return true;
+        for(int i=0;i<arr.size();i++){
+            if(key==arr[i]) return true;
+        }
+        return false;
     }
 };
 
