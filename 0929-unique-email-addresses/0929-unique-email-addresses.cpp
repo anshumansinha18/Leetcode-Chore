d15dcd2400a5b7c8200b79b01512d8578dbe7517
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-12-20 20:34:06 +0530

    Time: 24 ms (41.18%), Space: 14.1 MB (74.75%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.cpp
+++ 0929-unique-email-addresses/0929-unique-email-addresses.cpp
@@ -2,34 +2,31 @@ class Solution {
 public:
     int numUniqueEmails(vector<string>& emails) {
         
-        
         int n = emails.size();
-        
         set<string> s;
+        
         for(int i=0;i<n;i++){
-            string res="";
-           int j=0;
             
-           //before +
-            while(emails[i][j]!='+' && emails[i][j]!='@'){
-                if(emails[i][j]!='.')
-                    res+= emails[i][j];
+            int j=0;
+            string str="";
+            int size=emails[i].size();
+            
+            while(j<size && emails[i][j]!='+' && emails[i][j]!='@'){
+                if(emails[i][j]!='.') str+=emails[i][j];
+                j++;
+             }
+            
+            while(j<size && emails[i][j]!='@') j++;
+            
+            while(j<size){
+                str+=emails[i][j];
                 j++;
             }
-            
-            //from + to @
-            while(emails[i][j]!='@')
-                j++;
-            
-            //from @ to end
-            while(j<emails[i].size())
-               res+=emails[i][j++];
-            
-            s.insert(res);
+            // cout<<str<<endl;
+          s.insert(str);
         }
-        
-        
-        
-        return s.size();
+          return s.size();
     }
+        
+      
 };
