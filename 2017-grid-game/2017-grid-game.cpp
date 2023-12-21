71822abf0d4fdbf0f79461fc65af336d9e10377a
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-12-21 20:51:33 +0530

    Time: 132 ms (45.37%), Space: 75.2 MB (14.98%) - LeetHub

--- 2017-grid-game/2017-grid-game.cpp
+++ 2017-grid-game/2017-grid-game.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
-        
-         long long first = 0, second=0;
-         long long minSum=LLONG_MAX;
-        int n = grid[0].size();
-        
-         for(int i=0;i<n;i++){
-             first+=grid[0][i];
-         }
-        
-        
+        int n=grid.size();
+        int m = grid[0].size();
+        vector<vector<long long>> sum(n, vector<long long>(m,0));
         for(int i=0;i<n;i++){
-            first-=grid[0][i];
-            minSum = min(minSum, max(first, second));
-            second+=grid[1][i];
+            for(int j=0;j<m;j++){
+                if(j==0) sum[i][j]=grid[i][j];
+                else sum[i][j]=sum[i][j-1]+grid[i][j];
+            }
+        }
+        long long minScore=LLONG_MAX;
+        for(int i=0;i<m;i++){
+            if(i==0)
+                minScore = min(minScore, (sum[0][m-1]-sum[0][0]));
+            else if(i==m-1){
+                minScore = min(minScore, sum[1][i-1]);
+            }else{
+                minScore = min(minScore, max((sum[0][m-1]-sum[0][i]), (sum[1][i-1])));
+            }
         }
         
-        return minSum;
+        return minScore;
     }
 };
