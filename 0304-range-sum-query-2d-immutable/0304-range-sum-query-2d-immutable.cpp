c67925a281c1945de57dae0e7beea54c0c2dace3
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-12-22 13:06:59 +0530

    Time: 316 ms (72.37%), Space: 142.8 MB (79.74%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,37 +1,54 @@
 class NumMatrix {
-    vector<vector<int>> arr;
 public:
+    
+    vector<vector<int>> arr;
     NumMatrix(vector<vector<int>>& matrix) {
-        arr = matrix;
-        for(int i=0;i<arr.size();i++){
-            for(int j=1;j<arr[i].size();j++){
-                arr[i][j] = arr[i][j-1]+arr[i][j];
+        int m = matrix.size();
+        int n = matrix[0].size();
+        arr = vector<vector<int>>(m, vector<int>(n, 0));
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(j==0) arr[i][j]=matrix[i][j];
+                else arr[i][j]=arr[i][j-1]+matrix[i][j];
             }
         }
-
-        for(int j=0, k=j;j<arr[k].size();j++){
-            for(int i=1;i<arr.size();i++){
-                arr[i][j] = arr[i-1][j]+arr[i][j];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(j==0) arr[j][i]=arr[j][i];
+                else arr[j][i]=arr[j-1][i]+arr[j][i];
             }
         }
-
-        for(int i=0;i<arr.size();i++){
-            for(int j=0;j<arr[i].size();j++)
-                cout<<arr[i][j]<<" ";
-            cout<<endl;
-        }
-
+        
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<arr[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
     }
-
+    
     int sumRegion(int row1, int col1, int row2, int col2) {
-             int a = row1==0?0:arr[row1-1][col2];
-             int b = row1==0 || col1==0?0:arr[row1-1][col1-1];
-             int c = arr[row2][col2];
-             int d= col1==0?0:arr[row2][col1-1];
-
-             int e = a-b;
-             int f = c-d;
-             return f-e;
-
+        int a=0,b=0;
+        if(row1!=0)
+            a = arr[row2][col2]-arr[row1-1][col2];
+        else a = arr[row2][col2];
+        
+        if(col1!=0 && row1!=0){
+            b=arr[row2][col1-1]-arr[row1-1][col1-1];
+        }else if(col1!=0){
+            b=arr[row2][col1-1];
+        }
+        
+        
+        return a-b;
     }
 };
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
