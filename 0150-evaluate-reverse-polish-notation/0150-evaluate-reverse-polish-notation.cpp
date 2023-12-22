fc3780cb8e62aa09be2d057d4e50821ee9128a4c
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-12-22 23:43:56 +0530

    Time: 11 ms (36.83%), Space: 12.1 MB (94.22%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     
-    int performOp(int x, int y, string c){
-        if(c=="+")
-            return x+y;
-        else if(c=="-")
-            return x-y;
-        else if(c=="*")
-            return x*y;
-        else
-            return x/y;
+    bool isOperator(string c){
+        return c=="+" || c=="-" || c=="*" || c=="/";
+    }
+    
+    int performOp(stack<int>& st, string op){
+        int second = st.top();
+        st.pop();
+        int first = st.top();
+        st.pop();
+        
+        if(op=="+") return first+second;
+        else if(op=="-") return first-second;
+        else if(op=="*") return first*second;
+        else if(op=="/") return first/second;
+        else return -1;
     }
     int evalRPN(vector<string>& tokens) {
-        
-        int i=0;
         int n = tokens.size();
         stack<int> st;
-        while(i<n){
-            if(tokens[i]=="+" || tokens[i]=="-" || tokens[i]=="*" || tokens[i]=="/"){
-                int x = st.top();
-                st.pop();
-                int y = st.top();
-                st.pop();
-                st.push(performOp(y,x,tokens[i]));
+        for(int i=0;i<n;i++){
+            if(isOperator(tokens[i])){
+                int result = performOp(st, tokens[i]);
+                st.push(result);
             }else{
                 st.push(stoi(tokens[i]));
             }
-            i++;
         }
-        
         return st.top();
     }
 };
