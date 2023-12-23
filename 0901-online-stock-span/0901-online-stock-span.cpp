1574ecb0afde1148d90c83420bc67dd1b33d321c
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-12-23 14:23:50 +0530

    Time: 184 ms (22.56%), Space: 84.5 MB (56.68%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,33 +1,20 @@
 class StockSpanner {
-    stack<int> st;
-    vector<int> arr;
-    int i;
 public:
+    stack<pair<int, int>> st;
     StockSpanner() {
-          i=0;
+        
     }
     
     int next(int price) {
-       if(st.empty()){
-           st.push(i++);
-           arr.push_back(price);
-           return 1;
-       }else{
-           while(!st.empty() && price>=arr[st.top()]){
-               st.pop();
-           }
-         
-           arr.push_back(price);
-           int x = 1;
-           if(!st.empty()){
-             x = i-st.top();
-           }
-           else x = i+1;
-           st.push(i);
-           i++;
-           return x;
-       }
+        int count=1;
         
+        while(!st.empty() && price>=st.top().first){
+            count+=st.top().second;
+            st.pop();
+        }
+        
+        st.push(make_pair(price, count));
+        return st.top().second;
     }
 };
 
