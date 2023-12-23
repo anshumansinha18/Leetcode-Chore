9822183f31c6cfd09da868c9115b45c2206d4a8d
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-12-23 18:14:20 +0530

    Time: 6 ms (9.42%), Space: 11.4 MB (5.39%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,47 +1,67 @@
 class Solution {
 public:
-  bool isNumber(string c){
-    return (c[0]>='0' && c[0]<='9');
-  }
-
-string repeat(string rep, string count){
-    string result="";
-    int c=stoi(count);
-    for(int i=0;i<c;i++)
-        result+=rep;
-    return result;
-}
-
-string decodeString(string s) {
-
-    int n = s.size();
-    stack<string> st;
-    for(int i=0;i<n;i++){
-        if(s[i]==']'){
-            string str="";
-            //removing alphabets;
-            while(!st.empty() && st.top()!="["){
-                str = st.top()+str;
-                st.pop();
-            }
-            if(!st.empty()) st.pop();
-               //removing numbers:
-            string num="";
-            while(!st.empty() && isNumber(st.top())){
-                num=st.top()+num;
-                st.pop();
-            }
-             st.push(repeat(str, num));
-
-        }else{
-            string x = "";
-            x+=s[i];
-            st.push(x);
+    
+    bool isNumber(char c){
+        return c>='0' && c<='9';
+    }
+    
+    string repeat(int num, string str){
+        string rep="";
+        for(int i=0;i<num;i++){
+            rep+=str;
+        }
+        
+        return rep;
+    }
+    
+    void addToStack(string str, stack<char>& st){
+        int n = str.size();
+        for(int i=0;i<n;i++){
+            st.push(str[i]);
         }
     }
-    string result="";
-    while(!st.empty())
-        result = st.top()+result, st.pop();
-    return result;
-}
+    string decodeString(string s) {
+        
+        int n = s.size();
+        stack<char> st;
+        string temp="";
+        for(int i=0;i<n;i++){
+            
+            if(s[i]==']'){
+                //keep popping and creating a string until you see a [
+                string str = "";
+                
+                while(!st.empty() && st.top()!='['){
+                    str=st.top()+str;
+                    st.pop();
+                }
+                    
+                //pop([)
+                st.pop();     
+                //keep popping and creating a number until your stack becomes empty or 
+                //you find a character.
+                string num="";
+                while(!st.empty() && isNumber(st.top())){
+                    num=st.top()+num;
+                    st.pop();
+                }
+                
+                // cout<<str<<" "<<num<<endl;
+                
+                //perform operation.
+                string rep = repeat(stoi(num), str);
+                
+                addToStack(rep, st);
+            }else{
+                st.push(s[i]);
+            }
+        }
+        string result="";
+        while(!st.empty()){
+            result=st.top()+result;
+            st.pop();
+        }
+        return result;
+        
+    }
 };
