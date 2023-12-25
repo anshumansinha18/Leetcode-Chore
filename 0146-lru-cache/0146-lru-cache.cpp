1f1064cb3e2b38d061d5cd349ff6be773e29139d
Anshuman Sinha <39727166+anshumansinha18@users.noreply.github.com>
2023-12-25 15:06:15 +0530

    Time: 449 ms (7.84%), Space: 174.7 MB (39.77%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,45 +1,55 @@
 class LRUCache {
-    unordered_map<int, pair<list<int>::iterator, int>> m;
-    list<int> dll;
-    int size;
-    int capacity;
 public:
+    map<int,  list<pair<int, int>>::iterator> m;
+    list<pair<int,int>> dll;
+    int cap;
     LRUCache(int capacity) {
-        this->capacity=capacity;
-        this->size=0;
+        cap=capacity;
     }
     
     int get(int key) {
-        if(m.find(key)==m.end()) return -1;
         
-        int result = m[key].second;
-        dll.erase(m[key].first);
-        dll.push_front(key);
-        m[key].first = dll.begin();
-        return result;
+        if(m.find(key)==m.end()){
+            //if key doesnot exist
+            return -1;
+        }else{
+            //if key exists
+            int x = m[key]->second;
+            auto it=m[key];
+            dll.erase(it);
+            dll.push_front(make_pair(key, x));
+            m[key]=dll.begin();
+            return x;
+        }
+        
     }
     
     void put(int key, int value) {
-        
-        if(m.find(key)!=m.end()){
-            dll.erase(m[key].first);
-            dll.push_front(key);
-            m[key].first = dll.begin();
-            m[key].second=value;
-            return;
-        }
-        
-        dll.push_front(key);
-        m[key].first=dll.begin();
-        m[key].second=value;
-        size++;
-        
-        if(size>capacity){
-            list<int>:: iterator it = dll.end();
-            it--;
-            m.erase(*it);
+        //check if already exists in the map or not:
+        if(m.find(key)==m.end()){
+            //key doesnot exists
+            
+            if(m.size()==cap){
+                //if cache is full already
+                auto it = prev(dll.end());
+                int k = it->first;
+                dll.erase(it);
+                m.erase(k);
+                dll.push_front(make_pair(key, value));
+                m[key]=dll.begin();
+            }else{
+                //cache is not full
+                dll.push_front(make_pair(key, value));
+                m[key]=dll.begin();
+            }
+        }else{
+            //key exists
+            auto it = m[key];
             dll.erase(it);
+            dll.push_front(make_pair(key, value));
+            m[key]=dll.begin();
         }
+        
     }
 };
 
